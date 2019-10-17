a283701629978318de9db7a824cae5ba3dc13748
Jonathan M. Wilbur <jonathan@wilbur.space>
2019-10-16 21:07:06 -0400

    Add missing inner() test for DER

--- test/der/constructed.test.js
+++ test/der/constructed.test.js
@@ -104,4 +104,20 @@ describe("Distinguished Encoding Rules", function () {
         element.fromBytes(data);
         expect(() => element.printableString).toThrow();
     });
+
+    it("encodes and decodes explicitly-encoded elements correctly", () => {
+        const outerElement = new asn1.DERElement(
+            asn1.ASN1TagClass.context,
+            asn1.ASN1Construction.primitive,
+            5,
+        );
+        const innerElement = new asn1.DERElement(
+            asn1.ASN1TagClass.universal,
+            asn1.ASN1Construction.primitive,
+            asn1.ASN1UniversalType.objectIdentifier,
+            new asn1.ObjectIdentifier([ 1, 5, 7 ]),
+        );
+        outerElement.inner = innerElement;
+        expect(outerElement.inner.objectIdentifier.nodes).toEqual([ 1, 5, 7 ]);
+    });
 });
