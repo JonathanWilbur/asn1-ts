1165ceaa41fa5ac44644911d97c9f63a7cb6f7c1
Jonathan M. Wilbur <jonathan@wilbur.space>
2019-11-09 21:27:43 -0500

    Format TypeIdentifier

--- source/types/typeidentifier.ts
+++ source/types/typeidentifier.ts
@@ -1,17 +1,17 @@
 import { ASN1Element } from "../asn1";
 import { ObjectIdentifier } from "./objectidentifier";
 
-// From ITU X.681, Annex A.2:
-
-// TYPE-IDENTIFIER ::= CLASS {
-//     &id OBJECT IDENTIFIER UNIQUE,
-//     &Type }
-//     WITH SYNTAX {&Type IDENTIFIED BY &id}
-
+/**
+ * From ITU X.681, Annex A.2:
+ * `TYPE-IDENTIFIER ::= CLASS {
+ *     &id OBJECT IDENTIFIER UNIQUE,
+ *     &Type }
+ *     WITH SYNTAX {&Type IDENTIFIED BY &id}`
+ */
 export
 class TypeIdentifier<Element extends ASN1Element> {
     constructor (
         readonly id: ObjectIdentifier,
-        readonly type: Element
+        readonly type: Element,
     ) {}
 }
