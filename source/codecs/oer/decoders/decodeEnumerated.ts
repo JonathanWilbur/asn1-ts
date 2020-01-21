354a474a1445f6d3d5e1eaf8b2355a73e2985789
Jonathan M. Wilbur <jonathan@wilbur.space>
2020-01-21 03:38:37 -0500

    Use Macros in OER codec functions

--- source/codecs/oer/decoders/decodeEnumerated.ts
+++ source/codecs/oer/decoders/decodeEnumerated.ts
@@ -1,7 +1,8 @@
 import decodeBase128 from "../../../utils/decodeBase128";
 import decodeSignedBigEndianInteger from "../../../utils/decodeSignedBigEndianInteger";
+import { ENUMERATED } from "../../../macros";
 
 export default
-function decodeEnumerated (value: Uint8Array): number {
+function decodeEnumerated (value: Uint8Array): ENUMERATED {
     return decodeSignedBigEndianInteger(decodeBase128(value));
 }
--- source/codecs/oer/encoders/encodeEnumerated.ts
+++ source/codecs/oer/encoders/encodeEnumerated.ts
@@ -1,7 +1,8 @@
 import encodeBase128 from "../../../utils/encodeBase128";
 import encodeSignedBigEndianInteger from "../../../utils/encodeSignedBigEndianInteger";
+import { ENUMERATED } from "../../../macros";
 
 export default
-function encodeEnumerated (value: number): Uint8Array {
+function encodeEnumerated (value: ENUMERATED): Uint8Array {
     return encodeBase128(encodeSignedBigEndianInteger(value));
 }
