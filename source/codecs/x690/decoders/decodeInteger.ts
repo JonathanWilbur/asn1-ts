951e31b76e6a6adbbd997f73a7aebba0c1c1fd9b
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-08-04 14:58:02 -0400

    More detailed error message when INTEGER or ENUMERATED has padding

--- source/codecs/x690/decoders/decodeInteger.ts
+++ source/codecs/x690/decoders/decodeInteger.ts
@@ -5,7 +5,7 @@ import { bufferToInteger } from "../../../utils/bigint";
 export default
 function decodeInteger (value: Uint8Array): INTEGER {
     if (value.length === 0) {
-        throw new errors.ASN1SizeError("INTEGER encoded on zero bytes!");
+        throw new errors.ASN1SizeError("INTEGER or ENUMERATED encoded on zero bytes");
     }
     if (
         value.length > 2
@@ -14,7 +14,11 @@ function decodeInteger (value: Uint8Array): INTEGER {
             || (value[0] === 0x00 && value[1] < 0b10000000)
         )
     ) {
-        throw new errors.ASN1PaddingError("Unnecessary padding bytes on INTEGER or ENUMERATED.");
+        // We slice so nefarious users cannot inundate logs with gigantic arbitrary integers.
+        const buf = Buffer.from(value.slice(0, 16));
+        throw new errors.ASN1PaddingError(
+            "Unnecessary padding bytes on INTEGER or ENUMERATED. "
+            + `First 16 bytes of the offending value were: 0x${buf.toString("hex")}`);
     }
     return bufferToInteger(value);
 }
