51a29ffbf2ff203158290e5ba249989c697ac811
Jonathan M. Wilbur <jonathan@wilbur.space>
2022-08-09 08:34:44 -0400

    Interpret UTCTime per ITU X.509 (2019), Section 7.2.1.

--- source/codecs/der/decoders/decodeUTCTime.ts
+++ source/codecs/der/decoders/decodeUTCTime.ts
@@ -13,7 +13,16 @@ function decodeUTCTime (value: Uint8Array): UTCTime {
     }
     const ret: Date = new Date();
     let year: number = Number(match[1]);
-    year = (year < 70 ? (2000 + year) : (1900 + year));
+    /**
+     * The ITU specifications for ASN.1 and related codecs do not specify what
+     * century the year digits of a UTCTime value refers to. However, ITU
+     * Recommendation X.509 (2019), Section 7.2, states that the `utcTime`
+     * alternative of the `Time` type shall be interpreted as being year 20XX if
+     * XX is between 0 and 49 inclusive, and 19XX otherwise.
+     */
+    year = (year <= 49)
+        ? (2000 + year)
+        : (1900 + year);
     const month: number = (Number(match[2]) - 1);
     const date: number = Number(match[3]);
     const hours: number = Number(match[4]);
